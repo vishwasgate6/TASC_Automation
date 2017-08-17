ca3e6f15842cb780c9c3594855d1220a1626b428
Vishwas <Vishwas@TSD-124>
2017-08-17 16:20:14 +0530

    manage user chnages

--- src/TASC_Automation/Manage_User.java
+++ src/TASC_Automation/Manage_User.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 import java.util.List;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -11,8 +12,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.AfterTest;
 
 public class Manage_User {
 
@@ -35,16 +34,16 @@ public class Manage_User {
 		WebElement Pass = wait.until(ExpectedConditions.elementToBeClickable(By.id("Password")));
 		Pass.sendKeys("tags1234");
 		Thread.sleep(2000);
-		
+
 		WebElement clickLogin = wait.until(
 				ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='btn btn-large btn-fullwidth m-t40']")));
 		clickLogin.click();
 		Thread.sleep(3000);
 	}
 
-	 @Test (priority = 0)
+	@Test(priority = 0)
 	public void Addnewuser() throws InterruptedException {
-	
+
 		WebElement ClickAddNewUser = wait.until(ExpectedConditions.elementToBeClickable(By.id("btnAddNewUser")));
 		ClickAddNewUser.click();
 		Thread.sleep(2000);
@@ -68,85 +67,134 @@ public class Manage_User {
 		WebElement selectagency = driver.findElement((By.xpath("//div[2]/div/div/div/div[2]/div/span/span/span")));
 		selectagency.click();
 		Thread.sleep(2000);
-		
-		//select the agency from drop-down
+
+		// select the agency from drop-down
 		String searchText = "YUMA COUNTY ADULT PROBATION";
 		List<WebElement> options = driver.findElements(By.xpath("//ul[@id='Agency_listbox']/li"));
 		// Loop through the options and select the one that matches
 		for (WebElement option : options) {
 			if (option.getText().equals(searchText)) {
-				option.click(); // click the desired option
-				}
+				option.click();
+				// click the desired option
+			}
+
 		}
-		
-		WebElement Role = driver.findElement((By.xpath("//div[1]/div[2]/div[2]/div/div[3]/div/div/span[1]/span/span[2]/span")));
+
+		WebElement Role = driver
+				.findElement((By.xpath("//div[1]/div[2]/div[2]/div/div[3]/div/div/span[1]/span/span[2]/span")));
 		Role.click();
 		Thread.sleep(2000);
-		
-		//select the Roles from drop-down
+
+		// select the Roles from drop-down
 		String searchrole = "CASE MANAGER";
 		List<WebElement> roles = driver.findElements(By.xpath("//ul[@id='Role_listbox']/li"));
 		// Loop through the options and select the one that matches
 		for (WebElement searchroles : roles) {
 			if (searchroles.getText().equals(searchrole)) {
-				searchroles.click(); // click the desired option
+				searchroles.click();
+				// click the desired option
 				Thread.sleep(2000);
-				
+
 			}
+
 		}
-		
+
 		WebElement Phonenumber = wait.until(ExpectedConditions.elementToBeClickable(By.id("CellNo")));
 		Phonenumber.sendKeys("9713108197");
 		Thread.sleep(2000);
-		
+
 		WebElement clickSave = wait.until(ExpectedConditions.elementToBeClickable(By.id("btnSave")));
 		clickSave.click();
 		Thread.sleep(2000);
-		
+
 		WebElement GetText = wait.until(ExpectedConditions.elementToBeClickable(By.className("Success")));
 		String Alertmessage = GetText.getText();
 		Assert.assertEquals(Alertmessage, "User created successfully!");
 		System.out.println(Alertmessage);
 	}
 
-	
-	@Test (priority = 1)
-	public void validationuser () throws InterruptedException {
-		
+	@Test(priority = 1)
+	public void validationuser() throws InterruptedException {
+
 		// Without enter any data click on the create user button
-		
+
 		WebElement ClickAddNewUser = wait.until(ExpectedConditions.elementToBeClickable(By.id("btnAddNewUser")));
 		ClickAddNewUser.click();
 		Thread.sleep(2000);
-		
+
 		WebElement clickSave = wait.until(ExpectedConditions.elementToBeClickable(By.id("btnSave")));
 		clickSave.click();
-		//Thread.sleep(9000);
-		
+		// Thread.sleep(9000);
+
 		WebElement firstname = wait.until(ExpectedConditions.elementToBeClickable(By.id("FirstName")));
 		firstname.sendKeys("");
 		Thread.sleep(2000);
-		
+
 		WebElement alertmessage = wait
 				.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='field-validation-error']")));
 		String firstnamealertmessage = alertmessage.getText();
 		Assert.assertEquals(firstnamealertmessage, "The First Name is required field");
 		System.out.println(firstnamealertmessage);
-		
+
 		WebElement lastname = wait.until(ExpectedConditions.elementToBeClickable(By.name("LastName")));
 		lastname.sendKeys("");
-		
+
 		String lastnamealertmessage1 = "The Last Name is required field";
-		Assert.assertEquals(lastnamealertmessage1,"The Last Name is required field");
+		Assert.assertEquals(lastnamealertmessage1, "The Last Name is required field");
 		System.out.println(lastnamealertmessage1);
-				
+
+		WebElement selectagency = driver.findElement((By.xpath("//div[2]/div/div/div/div[2]/div/span/span/span")));
+		selectagency.sendKeys("");
+
+		String agencyalert = "The Agency is required field.";
+		Assert.assertEquals(agencyalert, "The Agency is required field.");
+		System.out.println(agencyalert);
+
+		String Emailalert = "The Email field is required.";
+		Assert.assertEquals(Emailalert, "The Email field is required.");
+		System.out.println(Emailalert);
+
+		String Role = "The Role is required field.";
+		Assert.assertEquals(Role, "The Role is required field.");
+		System.out.println(Role);
+
 	}
+
+	@Test(priority = 0)
+	public void Edituser() throws InterruptedException {
 		
-	
+		
+		WebElement filtertextbox = wait.until(ExpectedConditions.elementToBeClickable(By.id("FilterKeywords")));
+		filtertextbox.sendKeys("vishwas9011@gmail.com");
+		filtertextbox.sendKeys(Keys.ENTER);
+		Thread.sleep(2000);
+
+		String Emailcheck = "vishwas9011@gmail.com";
+		Assert.assertEquals(Emailcheck, "vishwas9011@gmail.com");
+		System.out.println(Emailcheck);
+
+		WebElement edituserclick = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='mCSB_2_container']/div[1]/table/tbody/tr/td[2]/a")));
+		edituserclick.click();
+		Thread.sleep(3000);
+		
+		//Edit middle name
+		WebElement Middlename = wait.until(ExpectedConditions.elementToBeClickable(By.id("MiddleName")));
+		Middlename.sendKeys("hello");
+		Thread.sleep(2000);
+		
+		WebElement clickSave = wait.until(ExpectedConditions.elementToBeClickable(By.id("btnSave")));
+		clickSave.click();
+		Thread.sleep(2000);
+
+		String Success = "User updated successfully!";
+		Assert.assertEquals(Success, "User updated successfully!");
+		System.out.println(Success);
+	}
+
 	@AfterClass
 	public void closeBrowser() throws InterruptedException {
 
-		//Thread.sleep(2000);
+		
 		//driver.close();
 	}
 

69eaac82ae6c66ec0c3027b70b54f072e25abdd2
TSD-124\Vishwas <vishwas.kalkar@gate6.com>
2017-10-03 17:09:04 +0530

    code update: Manage User: 10/03/2017

--- tasc/src/test/java/tasc/Manage_UserTest.java
+++ tasc/src/test/java/tasc/Manage_UserTest.java
@@ -1,5 +1,9 @@
 package tasc;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
 
@@ -15,6 +19,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.openqa.selenium.JavascriptExecutor;
 
 public class Manage_UserTest {
 
@@ -26,7 +31,7 @@ public class Manage_UserTest {
 	public void OpenBrowseAndLogin() throws InterruptedException {
 
 		driver.get("https://stage1.tascportal.org");
-		// driver.get("https://tascportal.org");
+		//driver.get("https://tascportal.org");
 		driver.manage().window().maximize();
 		Thread.sleep(4000);
 
@@ -49,44 +54,46 @@ public class Manage_UserTest {
 
 		WebElement ClickAddNewUser = wait.until(ExpectedConditions.elementToBeClickable(By.id("btnAddNewUser")));
 		ClickAddNewUser.click();
+		((JavascriptExecutor)driver).executeScript("scroll(0,400)");
 		Thread.sleep(2000);
 
 		WebElement FirstName = wait.until(ExpectedConditions.elementToBeClickable(By.id("FirstName")));
-		FirstName.sendKeys("hello");
+		FirstName.sendKeys("hello1");
 		Thread.sleep(2000);
 
 		WebElement MiddleName = wait.until(ExpectedConditions.elementToBeClickable(By.id("MiddleName")));
-		MiddleName.sendKeys("test");
+		MiddleName.sendKeys("test1");
 		Thread.sleep(2000);
 
 		WebElement LastName = wait.until(ExpectedConditions.elementToBeClickable(By.id("LastName")));
-		LastName.sendKeys("gate6");
+		LastName.sendKeys("gate61");
 		Thread.sleep(2000);
 
 		WebElement Email = wait.until(ExpectedConditions.elementToBeClickable(By.id("Email")));
-		Email.sendKeys("hello1@gate6.com");
+		Email.sendKeys("hello2@gate6.com");
 		Thread.sleep(2000);
 
-		WebElement selectagency = driver.findElement((By.xpath("//div[2]/div/div/div/div[2]/div/span/span/span")));
+		WebElement selectagency = driver.findElement((By.xpath("//*[@id='frmUserManagement']/div[1]/div[2]/div[2]/div/div[1]/div/div[2]/span/span/span[2]/span")));
 		selectagency.click();
 		Thread.sleep(2000);
 
 		// select the agency from drop-down
-		String searchText = "YUMA COUNTY ADULT PROBATION";
+		String searchText = "GATE6TESTQA";
 		List<WebElement> options = driver.findElements(By.xpath("//ul[@id='Agency_listbox']/li"));
 		// Loop through the options and select the one that matches
 		for (WebElement option : options) {
 			if (option.getText().equals(searchText)) {
 				option.click();
+				Thread.sleep(4000);
 				// click the desired option
 			}
 
 		}
 
-		WebElement Role = driver
-				.findElement((By.xpath("//div[1]/div[2]/div[2]/div/div[3]/div/div/span[1]/span/span[2]/span")));
+		WebElement Role = driver.findElement((By.xpath(
+				"//*[@id='frmUserManagement']/div[1]/div[2]/div[2]/div/div[3]/div/div/span[1]/span/span[2]/span")));
 		Role.click();
-		Thread.sleep(2000);
+		Thread.sleep(3000);
 
 		// select the Roles from drop-down
 		String searchrole = "CASE MANAGER";
@@ -96,8 +103,6 @@ public class Manage_UserTest {
 			if (searchroles.getText().equals(searchrole)) {
 				searchroles.click();
 				// click the desired option
-				Thread.sleep(3000);
-
 			}
 
 		}
@@ -112,11 +117,11 @@ public class Manage_UserTest {
 
 		WebElement GetText = wait.until(ExpectedConditions.elementToBeClickable(By.className("Success")));
 		String Alertmessage = GetText.getText();
-		Assert.assertEquals(Alertmessage, "User created successfully!");
+		AssertJUnit.assertEquals(Alertmessage, "User created successfully!");
 		System.out.println(Alertmessage);
 	}
 
-	@Test(priority = 1)
+	/*@Test(priority = 1)
 	public void validationuser() throws InterruptedException {
 
 		// Without enter any data click on the create user button
@@ -161,32 +166,35 @@ public class Manage_UserTest {
 		Assert.assertEquals(Role, "The Role is required field.");
 		System.out.println(Role);
 
-	}
+	}*/
 
-	@Test(priority = 2)
+	@Test(priority = 1)
 	public void Edituser() throws InterruptedException {
 
 		WebElement filtertextbox = wait.until(ExpectedConditions.elementToBeClickable(By.id("FilterKeywords")));
-		filtertextbox.sendKeys("test@gate634.com");
+		filtertextbox.sendKeys("hello2@gate6.com");
 		filtertextbox.sendKeys(Keys.ENTER);
+		((JavascriptExecutor)driver).executeScript("scroll(0,400)");
 		Thread.sleep(2000);
 
-		String actualTitle = driver.findElement(By.id("UserViewTitle")).getText();
-		String expectedTitle = "EDIT USER > USER, TEST";
-		assertEquals(actualTitle, expectedTitle);
-
-		String Emailcheck = "test@gate634.com";
-		Assert.assertEquals(Emailcheck, "test@gate634.com");
-		System.out.println(Emailcheck);
-
-		WebElement edituserclick = wait.until(ExpectedConditions
-				.elementToBeClickable(By.xpath("//*[@id='mCSB_2_container']/div[1]/table/tbody/tr/td[2]/a")));
+		WebElement edituserclick = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".grid-link")));
 		edituserclick.click();
 		Thread.sleep(3000);
 
+		String actualTitle = driver.findElement(By.id("UserViewTitle")).getText();
+		String expectedTitle = "EDIT USER > GATE61, HELLO1 TEST1";
+		AssertJUnit.assertEquals(actualTitle, expectedTitle);
+		Thread.sleep(2000);
+
+		String Emailcheck = "hello2@gate6.com";
+		AssertJUnit.assertEquals(Emailcheck, "hello2@gate6.com");
+		System.out.println(Emailcheck);
+		Thread.sleep(2000);
+
 		// Edit middle name
 		WebElement Middlename = wait.until(ExpectedConditions.elementToBeClickable(By.id("MiddleName")));
-		Middlename.sendKeys("hello");
+		Middlename.clear();
+		Middlename.sendKeys("Tasc");
 		Thread.sleep(2000);
 
 		WebElement clickSave = wait.until(ExpectedConditions.elementToBeClickable(By.id("btnSave")));
@@ -194,16 +202,17 @@ public class Manage_UserTest {
 		Thread.sleep(2000);
 
 		String Success = "User updated successfully!";
-		Assert.assertEquals(Success, "User updated successfully!");
+		AssertJUnit.assertEquals(Success, "User updated successfully!");
 		System.out.println(Success);
 	}
 
-	@Test(priority = 3)
+	@Test(priority = 2)
 	public void emailalreadyexist() throws InterruptedException {
 
 		WebElement filtertextbox = wait.until(ExpectedConditions.elementToBeClickable(By.id("FilterKeywords")));
-		filtertextbox.sendKeys("test@test456.com");
+		filtertextbox.sendKeys("hello2@gate6.com");
 		filtertextbox.sendKeys(Keys.ENTER);
+		((JavascriptExecutor)driver).executeScript("scroll(0,400)");
 		Thread.sleep(2000);
 
 		WebElement edituserclick = wait.until(ExpectedConditions.elementToBeClickable(By.className("grid-link")));
@@ -220,13 +229,13 @@ public class Manage_UserTest {
 		Thread.sleep(2000);
 
 		String alertmessage = "Email already Exist.";
-		Assert.assertEquals(alertmessage, "Email already Exist.");
+		AssertJUnit.assertEquals(alertmessage, "Email already Exist.");
 		System.out.println(alertmessage);
 		Thread.sleep(2000);
 
 	}
 
-	@Test(priority = 4)
+	@Test(priority = 3)
 	public void deleteuserwithoutdonor() throws InterruptedException {
 
 		WebElement selectagency = driver.findElement((By.className("k-select")));
@@ -263,9 +272,14 @@ public class Manage_UserTest {
 			}
 		}
 
-		WebElement clickuser = driver
-				.findElement(By.xpath("//*[@id='mCSB_2_container']/div[1]/table/tbody/tr/td[2]/a"));
-		clickuser.click();
+		WebElement filtertextbox = wait.until(ExpectedConditions.elementToBeClickable(By.id("FilterKeywords")));
+		filtertextbox.sendKeys("test@gate634.com");
+		filtertextbox.sendKeys(Keys.ENTER);
+		((JavascriptExecutor)driver).executeScript("scroll(0,400)");
+		Thread.sleep(2000);
+		
+		WebElement clickuser = driver.findElement(By.cssSelector(".grid-link"));
+		clickuser.click();		
 		Thread.sleep(2000);
 
 		WebElement deletebuttonclick = driver.findElement(By.id("btnDelete"));
@@ -273,35 +287,35 @@ public class Manage_UserTest {
 		Thread.sleep(2000);
 
 		String alertmessage = "Are you sure you want to delete?";
-		Assert.assertEquals(alertmessage, "Are you sure you want to delete?");
+		AssertJUnit.assertEquals(alertmessage, "Are you sure you want to delete?");
 		System.out.println(alertmessage);
 		Thread.sleep(2000);
 
-		WebElement btncancel = driver.findElement(By.xpath("//*[@id='btnClosePopup']"));
+		WebElement btncancel = driver.findElement(By.id("btnClosePopup"));
 		btncancel.click();
 		Thread.sleep(2000);
-		
+
 		WebElement deletebutton = driver.findElement(By.id("btnDelete"));
 		deletebutton.click();
 		Thread.sleep(2000);
-		
+
 		WebElement confirmbuttonclick = driver.findElement(By.xpath("//*[@id='btnConfirm']"));
 		confirmbuttonclick.click();
 		Thread.sleep(2000);
-		
+
 		String Success = "User deleted successfully!";
-		Assert.assertEquals(Success, "User deleted successfully!");
+		AssertJUnit.assertEquals(Success, "User deleted successfully!");
 		System.out.println(Success);
 
 	}
 
-	@Test(priority = 5)
-	public void deleteuserwithtdonor() throws InterruptedException {
-		
+	@Test(priority = 4)
+	public void deleteuserwithdonor() throws InterruptedException {
+
 		WebElement selectagency = driver.findElement((By.className("k-select")));
 		selectagency.click();
 		Thread.sleep(2000);
-		
+
 		String searchText = "YUMA COUNTY ADULT PROBATION";
 		List<WebElement> options = driver.findElements(By.xpath("//ul[@id='AgencyFilter_listbox']/li"));
 
@@ -330,7 +344,7 @@ public class Manage_UserTest {
 				Thread.sleep(3000);
 			}
 		}
-		
+
 		WebElement status = driver.findElement((By.xpath(
 				"//*[@id='body']/section/div/div[1]/div[1]/div/div/div/div[2]/div/div[3]/div/span/span/span[2]/span")));
 		status.click();
@@ -339,7 +353,7 @@ public class Manage_UserTest {
 		// select the Roles from drop-down
 		String statusfilter1 = "Active";
 		List<WebElement> filterstatus = driver.findElements(By.xpath("//ul[@id='StatusFilter_listbox']/li"));
-	
+
 		// Loop through the options and select the one that matches
 		for (WebElement status1 : filterstatus) {
 			if (status1.getText().toLowerCase().equals(statusfilter1.toLowerCase())) {
@@ -348,14 +362,28 @@ public class Manage_UserTest {
 				Thread.sleep(2000);
 			}
 		}
-		
+
 		WebElement filtertextbox = wait.until(ExpectedConditions.elementToBeClickable(By.id("FilterKeywords")));
+		filtertextbox.clear();
 		filtertextbox.sendKeys("vishwas9011@gmail.com");
 		filtertextbox.sendKeys(Keys.ENTER);
+		((JavascriptExecutor)driver).executeScript("scroll(0,400)");
 		Thread.sleep(3000);
 
+		WebElement clickuser = driver.findElement(By.className("grid-link"));
+		clickuser.click();
+		Thread.sleep(3000);
+
+		WebElement deletebutton = driver.findElement(By.id("btnDelete"));
+		deletebutton.click();
+		Thread.sleep(2000);
+
+		System.out.println(driver.findElement(By.xpath("//*[@id='tasc-popup-content']")).getText());
+		driver.findElement(By.xpath(".//*[@id='btnOk']")).click(); // for clicking the alert pop-up
+		Thread.sleep(2000);
+
 	}
-		
+
 	@AfterClass
 	public void closeBrowser() throws InterruptedException {
 

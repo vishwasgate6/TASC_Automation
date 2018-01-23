a2478cd113b6ba752627cf8318d479402fe4a668
TSD-124\Vishwas <vishwas.kalkar@gate6.com>
2018-01-23 17:22:58 +0530

    updates : Manage Caseload Section (23rd Jan 2018)

--- ManageCaseload/src/test/java/ManageCaseload/ManageCaseloadTest.java
+++ ManageCaseload/src/test/java/ManageCaseload/ManageCaseloadTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -100,7 +99,7 @@ public class ManageCaseloadTest {
 		Thread.sleep(2000);
 		
 		WebElement selecttouser = driver.findElement((By
-				.xpath(".//*[@id='CaseloadManagement']/section/div[1]/div/div[3]/div[3]/div[2]/div/div/span/span/span[2]/span")));
+				.xpath("//*[@id='CaseloadManagement']/section/div[1]/div/div[3]/div[3]/div[2]/div/div/span/span/span[2]/span")));
 		selecttouser.click();
 		Thread.sleep(2000);
 		
@@ -126,20 +125,14 @@ public class ManageCaseloadTest {
 		System.out.println(Alertmessage3);
 		Thread.sleep(2000);
 	
-//		driver.findElement(By.xpath("//label[@for='8e0458e9-d810-4cc5-9886-bf80449daba3']")).click();
-//		Thread.sleep(3000);
-//		if (!driver.findElement(By.id("8e0458e9-d810-4cc5-9886-bf80449daba3")).isSelected()) {
-//			Thread.sleep(3000);
-//        	driver.findElement(By.id("8e0458e9-d810-4cc5-9886-bf80449daba3")).click();
-//        	Thread.sleep(3000);
-//         }
 	}
 	
 	@Test(priority = 2)
 	public void NoCaseloadfound() throws InterruptedException {
 		
-		
-		
+		driver.get("https://stage1.tascportal.org/CaseManager/CaseLoadManagement");
+		driver.navigate().refresh();
+		Thread.sleep(3000);
 		
 		WebElement selectagency = driver.findElement((By
 				.xpath("//*[@id='CaseloadManagement']/section/div[1]/div/div[2]/div/div/div/span/span/span[2]/span")));
@@ -157,8 +150,13 @@ public class ManageCaseloadTest {
 		}
 		Thread.sleep(3000);
 		
+		WebElement selecttouser = driver.findElement((By
+				.xpath("//*[@id='CaseloadManagement']/section/div[1]/div/div[3]/div[1]/div[2]/div/div/span/span/span[2]/span")));
+		selecttouser.click();
+		Thread.sleep(2000);
+		
 		String searchuser = "WORTHEN, CINDY";
-		List<WebElement> users = driver.findElements(By.xpath("//ul[@id='CaseManagerFrom_listbox']/li"));
+		List<WebElement> users = driver.findElements(By.xpath("//ul[@id='CaseManagerTo_listbox']/li"));
 		// Loop through the options and select the one that matches
 		for (WebElement option : users) {
 			if (option.getText().toLowerCase().equals(searchuser.toLowerCase())) {
@@ -171,11 +169,127 @@ public class ManageCaseloadTest {
 		WebElement alertmessage = wait.until(ExpectedConditions
 				.elementToBeClickable(By.xpath("//*[@id='grdTo']/div")));
 		String Alertmessage = alertmessage.getText();
-		Assert.assertEquals(Alertmessage, "No Caseload Available for this Case Manager.");
+		Assert.assertEquals(Alertmessage, "No Caseload Available For This Case Manager.");
 		System.out.println(Alertmessage);
 		Thread.sleep(2000);
 	}
+	
+	@Test(priority = 3)
+	public void checkatleastonecheckbox() throws InterruptedException {
+		
+		WebElement selectfromuser = driver.findElement((By
+				.xpath("//*[@id='CaseloadManagement']/section/div[1]/div/div[3]/div[1]/div[2]/div/div/span/span/span[2]/span")));
+		selectfromuser.click();
+		Thread.sleep(2000);
+		
+		String searchfromuser = "CM1, TEST1";
+		List<WebElement> fromusers = driver.findElements(By.xpath("//ul[@id='CaseManagerTo_listbox']/li"));
+		// Loop through the options and select the one that matches
+		for (WebElement option : fromusers) {
+			if (option.getText().toLowerCase().equals(searchfromuser.toLowerCase())) {
+				option.click();
+				
+			}
+		}
+		Thread.sleep(3000);
+		
 
+		WebElement selecttouser = driver.findElement((By
+				.xpath("//*[@id='CaseloadManagement']/section/div[1]/div/div[3]/div[3]/div[2]/div/div/span/span/span[2]/span")));
+		selecttouser.click();
+		Thread.sleep(2000);
+		
+		String searchtouser = "CM, DEFAULT";
+		List<WebElement> tousers = driver.findElements(By.xpath("//ul[@id='CaseManagerFrom_listbox']/li"));
+		// Loop through the options and select the one that matches
+		for (WebElement option : tousers) {
+			if (option.getText().toLowerCase().equals(searchtouser.toLowerCase())) {
+				option.click();
+				
+			}
+		}
+		Thread.sleep(3000);
+		
+		WebElement shiftrightbuttonclick = driver.findElement((By.xpath("//*[@id='btnShiftRight']")));
+		shiftrightbuttonclick.click();
+		Thread.sleep(2000);
+		
+		WebElement alertpopup = driver.findElement(By.xpath("//*[@id='tasc-popup-content']"));
+		String alertpopupmessage = alertpopup.getText();
+		System.out.println(alertpopupmessage);
+		Thread.sleep(2000);
+		
+		WebElement okbuttonclick = driver.findElement(By.id("btnOk"));
+		okbuttonclick.click();
+		Thread.sleep(2000);
+	}
+	
+	@Test(priority = 4)
+	public void donortransferlefttoright() throws InterruptedException {
+		
+		driver.findElement(By.xpath("//label[@for='8e0458e9-d810-4cc5-9886-bf80449daba3']")).click();
+		Thread.sleep(3000);
+		if (!driver.findElement(By.id("8e0458e9-d810-4cc5-9886-bf80449daba3")).isSelected()) {
+			Thread.sleep(3000);
+        	driver.findElement(By.id("8e0458e9-d810-4cc5-9886-bf80449daba3")).click();
+        	Thread.sleep(3000);
+         }
+		
+		WebElement shiftrightbuttonclick = driver.findElement((By.xpath("//*[@id='btnShiftRight']")));
+		shiftrightbuttonclick.click();
+		Thread.sleep(2000);
+		
+	}
+	
+	@Test(priority = 5)
+	public void donortransferrighttoleft() throws InterruptedException {
+		
+		driver.findElement(By.xpath("//label[@for='8e0458e9-d810-4cc5-9886-bf80449daba3']")).click();
+		Thread.sleep(3000);
+        if (!driver.findElement(By.id("8e0458e9-d810-4cc5-9886-bf80449daba3")).isSelected()) {
+        	Thread.sleep(3000);
+            driver.findElement(By.id("8e0458e9-d810-4cc5-9886-bf80449daba3")).click();
+            Thread.sleep(3000);
+        }
+        driver.findElement(By.id("btnShiftLeft")).click();
+        Thread.sleep(3000);
+        
+//        WebElement alertmessage = wait.until(ExpectedConditions
+//				.elementToBeClickable(By.xpath("//*[@id='grdFrom']/div")));
+//		String Alertmessage = alertmessage.getText();
+//		Assert.assertEquals(Alertmessage, "No Caseload Available For This Case Manager.");
+//		System.out.println(Alertmessage);
+//		Thread.sleep(2000);
+//		
+	}
+	
+	@Test(priority = 6)
+	public void caseloadtransfer() throws InterruptedException {
+		
+		driver.findElement(By.xpath("//label[@for='8e0458e9-d810-4cc5-9886-bf80449daba3']")).click();
+		Thread.sleep(3000);
+		if (!driver.findElement(By.id("8e0458e9-d810-4cc5-9886-bf80449daba3")).isSelected()) {
+			Thread.sleep(3000);
+        	driver.findElement(By.id("8e0458e9-d810-4cc5-9886-bf80449daba3")).click();
+        	Thread.sleep(3000);
+         }
+		
+		WebElement shiftrightbuttonclick = driver.findElement((By.xpath("//*[@id='btnShiftRight']")));
+		shiftrightbuttonclick.click();
+		Thread.sleep(2000);
+		
+		driver.findElement(By.id("btnSave")).click();
+		
+		 WebElement alertmessage = wait.until(ExpectedConditions
+					.elementToBeClickable(By.xpath("//*[@id='dvMSG']/div")));
+			String Alertmessage = alertmessage.getText();
+			Assert.assertEquals(Alertmessage, "This caseload transfer will occur in the background. Casemanager will move end of the day.");
+			System.out.println(Alertmessage);
+			Thread.sleep(2000);
+		//This caseload transfer will occur in the background. Casemanager will move end of the day.
+		
+	}
+	
 	@AfterClass
 	public void closeBrowser() throws InterruptedException {
 
